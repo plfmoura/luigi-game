0d4fd3dd5992eb57a940c069d35031680593137c
Pedro Moura <107577096+plfmoura@users.noreply.github.com>
2022-11-15 13:47:59 -0300

    add(script/style)score, title, and others

--- scripts/luigi-script.js
+++ scripts/luigi-script.js
@@ -1,38 +1,85 @@
+//personagens
 const luigi = document.querySelector('.luigi');
 const enemy = document.querySelector('.enemy');
+//modal
 const modal = document.querySelector('.die-modal');
-const score = document.querySelector('.score').innerHTML;
+const dieText = document.querySelector('.die-alert');
+//Hud game
+const startBtn = document.querySelector('.start-btn');
+const score = document.querySelector('.score');
+const textoInicio = document.querySelector('.txt-inicio');
 
-const jump = () => {
-    luigi.classList.add('jump');
+enemy.style.display = 'none';
 
-    setTimeout(() => {
-        luigi.classList.remove('jump');
-    },  600);
-}
+startBtn.addEventListener('click', () => {
+    //tirar o botão quando iniciar o jogo.
+    startBtn.style.display = 'none';
+    //pontuação
+    var pulos = 1;
+    function pontuacao() {
+            
+        var jump = addEventListener('keyup', function(e) {
+            var codigoTecla = e.which || e.keyCode || 0;
+            var space = codigoTecla == 32;
+            if(space){
+                score.innerHTML = pulos++;
+                if (pulos > 0){
+                    let pulosTotais = pulos -1;
+                    dieText.innerHTML = `Você Perdeu! <br>pontuanção: ${pulosTotais}`;
+                }
+            } 
+            });
+        }
 
-const loop = setInterval(() => {
+    pontuacao();
 
-    const enemyPosition = enemy.offsetLeft;
-    const luigiPosition = +window.getComputedStyle(luigi).bottom.replace('px','');
+    //tirar o texto quando começar a jogar
+    textoInicio.classList.add('txt-none');
+    //voltar visibilidade do inimigo quando começar jogo
+    enemy.style.display = 'block';
     
-    if(enemyPosition <= 60 && enemyPosition > 1 && luigiPosition < 50) {
-        enemy.style.animation = 'none';
-        enemy.style.left = `${enemyPosition}px`;
-
-        luigi.style.animation = 'none';
-        luigi.style.left = `${luigiPosition}px`;
-
-        luigi.src = 'assets/die.png';
-        luigi.style.width = '70px'
-        luigi.style.marginLeft = '12px'
-
-        clearInterval('loop');
-        
-        modal.classList.add('die-alert-on');
-        enemy.style.visibility = 'hidden';
+    //pulo
+    const jump = () => {
+        luigi.classList.add('jump');
+    
+        setTimeout(() => {
+            luigi.classList.remove('jump');
+        },  600);
     }
 
-    }, 10);
+    //jogo em si
+    const loop = setInterval(() => {
+    
+        const enemyPosition = enemy.offsetLeft;
+        const luigiPosition = +window.getComputedStyle(luigi).bottom.replace('px','');
+        
+        if(enemyPosition <= 60 && enemyPosition > 1 && luigiPosition < 50) {
+            enemy.style.animation = 'none';
+            enemy.style.left = `${enemyPosition}px`;
+    
+            luigi.style.animation = 'none';
+            luigi.style.left = `${luigiPosition}px`;
+    
+            luigi.src = 'assets/die.png';
+            luigi.style.width = '70px'
+            luigi.style.marginLeft = '12px'
+    
+            clearInterval('loop');
+            
+            modal.classList.add('die-alert-on');
+            enemy.style.visibility = 'hidden';
 
-    document.addEventListener('keypress', jump);
+            if(modal.classList.contains('die-alert-on')){
+                startBtn.style.pointerEvents = 'none';
+            }
+        }
+    
+        }, 10);
+        addEventListener('keyup', (e) => {
+            var codigoTecla = e.which || e.keyCode || 0;
+            var space = codigoTecla == 32;
+            if(space){
+                jump();
+            }});
+
+});
--- scripts/page.js
+++ scripts/page.js
@@ -2,4 +2,4 @@ const refresh = document.querySelector('.reload');
 
 refresh.addEventListener('click', () => {
     window.location.reload();
-})
+});
